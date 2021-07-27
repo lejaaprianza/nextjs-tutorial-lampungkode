3eaff894b29ca581125e680c29331fcdf7039fa1
lejaaprianza <lejaprianza@gmail.com>
2021-07-27 09:50:15 +0700

    tutorial-2

---
+++ pages/lampungkode.js
@@ -0,0 +1,7 @@
+export default function lampungkode() {
+  return (
+    <div>
+      <h1>Halaman Lampung Kode</h1>
+    </div>
+  );
+}
---
+++ pages/users/kode.js
@@ -0,0 +1,7 @@
+export default function Kode() {
+  return (
+    <div>
+      <h1>Ini Kode</h1>
+    </div>
+  );
+}
